24b6b528c13d85e8ca37f0f90c124da3e03a7ef7
Scott Green <sagreenxyz@gmail.com>
2022-02-22 13:40:08 -0600

    Functionality before bonus

---
+++ index.js
@@ -0,0 +1,7 @@
+const snakeNames = require('snake-names');
+
+let namePicked = snakeNames.random();
+
+console.log('You should name this snake: ', namePicked);
+
+// in console, run command "node index.js" to see results

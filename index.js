d285c7b5bc9339d9e590b600f286c9c23d31eff7
Scott Green <sagreenxyz@gmail.com>
2022-02-22 13:42:46 -0600

    With bonus functionality

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ let namePicked = snakeNames.random();
 console.log('You should name this snake: ', namePicked);
 
 // in console, run command "node index.js" to see results
+
+let femaleSnakeNames = snakeNames.female;
+let randomIndex = Math.floor(Math.random() * femaleSnakeNames.length);
+console.log("A great female snake name is: ", femaleSnakeNames[randomIndex]);
